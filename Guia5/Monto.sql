a4bc31ed95cc99c57b8d1a2c4c6adf87d3202ed3
JaelMelgar <jaelabigailmelgarortiz2@gmail.com>
2023-10-14 05:53:21 -0600

    Subiendo guia 5

---
+++ Guia5/Monto.sql
@@ -0,0 +1,19 @@
+﻿-- Crear una funci�n de tabla en l�nea para calcular el monto total de una venta
+CREATE FUNCTION dbo.CalcularMontoTotalVenta (@idVenta INT)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT SUM(P.Precio * DV.Cantidad) AS MontoTotal
+    FROM DetalleVentas DV
+    INNER JOIN Productos P ON DV.idProducto = P.idProducto
+    WHERE DV.idVenta = @idVenta
+);
+
+-- Ejemplo de uso de la funci�n de tabla en l�nea
+go
+DECLARE @idVenta INT;
+SET @idVenta = 1; -- Reemplaza esto con el ID de la venta que deseas calcular
+
+SELECT MontoTotal
+FROM dbo.CalcularMontoTotalVenta(@idVenta);
---
+++ Guia5/Precio.sql
@@ -0,0 +1,16 @@
+﻿-- Crear una funci�n de tabla para obtener productos con precio superior a un valor espec�fico
+CREATE FUNCTION dbo.ObtenerProductosPorPrecio (
+    @PrecioMinimo DECIMAL(10, 2)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT Nombre, Descripcion, Precio, Stock
+    FROM Productos
+    WHERE Precio > @PrecioMinimo
+);
+
+
+-- Llama a la funci�n con un valor espec�fico para @PrecioMinimo
+SELECT * FROM dbo.ObtenerProductosPorPrecio(12.00);
